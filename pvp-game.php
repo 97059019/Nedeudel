4200a83404eef868f5bc549e34dc2d4bdf57a5a7
Elmar Gerrits <89910698+97059019@users.noreply.github.com>
2023-10-17 15:00:23 +0200

    PvP
    front-end grootste gedeelte.
    setup voor backend gedaan, nog niet begonnen

---
+++ pvp-game.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100&family=Kanit:wght@100&display=swap"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Nedeudle</title>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+            <!-- First 6 Rows -->
+            <div class="row team1">
+    <div class="row-12">
+        <div id="team1guess1" class="row-12">
+            <a>1:</a>
+            <div class="team1guess1-1"><p>0</p></div>
+            <div class="team1guess1-2"><p>0</p></div>
+            <div class="team1guess1-3"><p>0</p></div>
+            <div class="team1guess1-4"><p>0</p></div>
+            <div class="team1guess1-5"><p>0</p></div>
+        </div>
+    </div>
+    <div class="row-12">
+      
+        <div id="team1guess2" class="row-12">
+            <a>2:</a>
+            <div class="team1guess1-1"><p>0</p></div>
+            <div class="team1guess2-2"><p>0</p></div>
+            <div class="team1guess2-3"><p>0</p></div>
+            <div class="team1guess2-4"><p>0</p></div>
+            <div class="team1guess2-5"><p>0</p></div>
+        </div>
+    </div>
+    <div class="col-12">
+        <div id="team1guess3"></div>
+            <a>3:</a>
+            <div class="team1guess3-1"><p>0</p></div>
+            <div class="team1guess3-2"><p>0</p></div>
+            <div class="team1guess3-3"><p>0</p></div>
+            <div class="team1guess3-4"><p>0</p></div>
+            <div class="team1guess3-5"><p>0</p></div>
+    </div>
+    <div class="col-12">   
+        <div id="team1guess4"></div>
+        <a>4:</a>
+            <div class="team1guess4-1"><p>0</p></div>
+            <div class="team1guess4-2"><p>0</p></div>
+            <div class="team1guess4-3"><p>0</p></div>
+            <div class="team1guess4-4"><p>0</p></div>
+            <div class="team1guess4-5"><p>0</p></div>
+    </div>
+    <div class="col-12">
+        <div id="team1guess5"></div>
+        <a>5:</a>
+            <div class="team1guess4-1"><p>0</p></div>
+            <div class="team1guess4-2"><p>0</p></div>
+            <div class="team1guess4-3"><p>0</p></div>
+            <div class="team1guess4-4"><p>0</p></div>
+            <div class="team1guess4-5"><p>0</p></div>
+    </div>
+    <div class="col-12">
+        <div id="team1guess6"></div>
+        <a>6:</a>
+            <div class="team1guess4-1"><p>0</p></div>
+            <div class="team1guess4-2"><p>0</p></div>
+            <div class="team1guess4-3"><p>0</p></div>
+            <div class="team1guess4-4"><p>0</p></div>
+            <div class="team1guess4-5"><p>0</p></div>
+    </div>
+</div>
+
+            <div class="row-6 input1">
+                    <div class="row-6 inputBox1">
+                    <input type="text" class="team1Letter" maxlength="1" >
+                    <input type="text" class="team1Letter" maxlength="1">
+                    <input type="text" class="team1Letter" maxlength="1">
+                    <input type="text" class="team1Letter" maxlength="1">
+                    <input type="text" class="team1Letter" maxlength="1">
+                    </div>
+                    <input type="button" class="team2btn" value="Send">
+                    
+                </div>
+
+        </div>
+        <div class="col-6">
+            <!-- Second 6 Rows -->
+            <div class="row team1">
+                <div class="col-12">
+                    <a>1:</a>
+                    <div class="team1guess1-1"><p>0</p></div>
+                    <div class="team1guess1-2"><p>0</p></div>
+                    <div class="team1guess1-3"><p>0</p></div>
+                    <div class="team1guess1-4"><p>0</p></div>
+                    <div class="team1guess1-5"><p>0</p></div>
+                </div>
+                <div class="col-12">
+                    <a>2:</a>
+                    <div class="team1guess2-1"><p>0</p></div>
+                    <div class="team1guess2-2"><p>0</p></div>
+                    <div class="team1guess2-3"><p>0</p></div>
+                    <div class="team1guess2-4"><p>0</p></div>
+                    <div class="team1guess2-5"><p>0</p></div>
+                </div>
+                <div class="col-12">
+                    <a>3:</a>
+                    <div class="team1guess3-1"><p>0</p></div>
+                    <div class="team1guess3-2"><p>0</p></div>
+                    <div class="team1guess3-3"><p>0</p></div>
+                    <div class="team1guess3-4"><p>0</p></div>
+                    <div class="team1guess3-5"><p>0</p></div>
+                </div>
+                <div class="col-12">
+                    <a>4:</a>
+                    <div class="team1guess4-1"><p>0</p></div>
+                    <div class="team1guess4-2"><p>0</p></div>
+                    <div class="team1guess4-3"><p>0</p></div>
+                    <div class="team1guess4-4"><p>0</p></div>
+                    <div class="team1guess4-5"><p>0</p></div>
+                </div>
+                <div class="col-12">
+                    <a>5:</a>
+                    <div class="team1guess5-1"><p>0</p></div>
+                    <div class="team1guess5-2"><p>0</p></div>
+                    <div class="team1guess5-3"><p>0</p></div>
+                    <div class="team1guess5-4"><p>0</p></div>
+                    <div class="team1guess5-5"><p>0</p></div>
+                </div>
+                <div class="col-12">
+                    <a>6:</a>
+                    <div class="team1guess6-1"><p>0</p></div>
+                    <div class="team1guess6-2"><p>0</p></div>
+                    <div class="team1guess6-3"><p>0</p></div>
+                    <div class="team1guess6-4"><p>0</p></div>
+                    <div class="team1guess6-5"><p>0</p></div>
+                </div>
+            </div>
+                <div class="row-6 input2">
+                    <div class="row-6 inputBox2">
+                    <input type="text" class="team2Letter" maxlength="1">
+                    <input type="text" class="team2Letter" maxlength="1">
+                    <input type="text" class="team2Letter" maxlength="1">
+                    <input type="text" class="team2Letter" maxlength="1">
+                    <input type="text" class="team2Letter" maxlength="1">
+                    </div>
+                    <input type="button" class="team2btn" value="Send">
+
+                </div>
+
+        </div>
+    </div>  
+
+
+
+
+</div>
+
+
+
+             
+</body>
+</html>
